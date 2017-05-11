854efa3a074e2306c876e69138f5e76ca1d73a7b
Jon Huh <oneandwholly@gmail.com>
2017-05-11 21:37:07 +0900

    fetch posts in PostsIndex component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
-export default class PostsIndex extends Component {
+class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
+    console.log(this.props.posts);
+
     return  (
       <div>
         Posts Index
       </div>
     )
-  }  }
+  }
+}
+
+function mapStateToProps ({ posts }) {
+  return { posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
