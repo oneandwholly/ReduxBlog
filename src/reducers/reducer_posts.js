4f309414a2a6329d5700b905a6bff97287a1ab9e
Jon Huh <oneandwholly@gmail.com>
2017-05-12 19:59:40 +0900

    finish wiring up PostsShow component

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,13 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 import _ from 'lodash';
 
 export default function PostsReducer(state = {}, action) {
   switch(action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case FETCH_POST:
+      const post = action.payload.data;
+      return { ...state, [post.id]: post }
     default:
       return state;
   }
