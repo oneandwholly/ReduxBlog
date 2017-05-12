8f92febb9de5e33d2f6ae69397c1d0870e73ff16
Jon Huh <oneandwholly@gmail.com>
2017-05-12 18:13:24 +0900

    add conditional rendering for the error messages for the PostsNew form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,23 +3,34 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+
+    const className = `form-group ${touched && error ? 'has-danger' : '' }`
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
       </div>
     );
   }
 
-
+  onSubmit(values) {
+    console.log(values);
+  }
 
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
@@ -35,6 +46,7 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
@@ -42,7 +54,7 @@ class PostsNew extends Component {
 
 function validate(values) {
   const errors = {};
-  
+
   if (!values.title) {
     errors.title = "Enter a title!";
   }
