fb5447e3c365c712e386c077d884df5d2eb1e47e
Jon Huh <oneandwholly@gmail.com>
2017-05-12 16:06:18 +0900

    add posts_new component for add a new blog post and wire up routing with react-router-dom

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 import _ from 'lodash';
 
@@ -23,6 +24,11 @@ class PostsIndex extends Component {
 
     return  (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
